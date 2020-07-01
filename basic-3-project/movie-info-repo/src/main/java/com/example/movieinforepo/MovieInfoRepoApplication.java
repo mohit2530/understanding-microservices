93d9b8591bfdb0d895c1490c7effcd4616e7f2f7
mohitpaudyal <mohit.paudyal@gmail.com>
2020-06-30 21:24:47 -0400

    Movie Info Repo - Architecture Complete

---
+++ basic-3-project/movie-info-repo/src/main/java/com/example/movieinforepo/MovieInfoRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.movieinforepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieInfoRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieInfoRepoApplication.class, args);
+	}
+
+}
---
+++ basic-3-project/movie-info-repo/src/test/java/com/example/movieinforepo/MovieInfoRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movieinforepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
